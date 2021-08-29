4b0887b462aae72baa04e02ec40bdadf92046c10
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-08-29 10:16:14 +0300

    Web Map of Volcanoes in USA and Population with Python

--- 02.Project/main.py
+++ 02.Project/main.py
@@ -1,12 +1,49 @@
 import folium
+import pandas
 
-location = [42.698334,23.319941]
-location_marker = [42.698350,23.319950]
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data['ELEV'])
+name = list(data["NAME"])
+
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+
+location = [38.58, -99.09]
 map = folium.Map(location=location, zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name='My Map')
-fg.add_child(folium.Marker(location=location_marker, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+fgv = folium.FeatureGroup(name='Volcanoes')
 
-map.add_child(fg)
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=150, height=80)
+    fgv.add_child(
+        folium.CircleMarker(location=(lt, ln), radius=6, popup=folium.Popup(iframe), fill_color=color_producer(el),
+                            color='green', fill_opacity=0.7))
+
+fgp = folium.FeatureGroup(name='Population')
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+                            style_function=lambda x: {'fillColor': 'green'
+                            if x['properties']['POP2005'] < 10000000
+                            else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
